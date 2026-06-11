6c3700be4544a5bba7a1dfa9953228723fc21c1d
devpedro404 <darknumber65@gmail.com>
2026-06-11 12:54:31 -0300

    test: removendo BreakingNewsTicker para teste

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import TopAppBar from './TopAppBar';
 import Footer from './Footer';
-import BreakingNewsTicker from './BreakingNewsTicker';
+// import BreakingNewsTicker from './BreakingNewsTicker';  // COMENTE POR ENQUANTO
 import DrawerMenu from './DrawerMenu';
 import ReadingProgress from './ReadingProgress';
 
@@ -20,17 +20,16 @@ const Layout = ({ children }) => {
       <TopAppBar onMenuClick={() => setIsDrawerOpen(true)} />
       <DrawerMenu isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
       
-      {/* Espaçador para compensar o header fixed */}
-      <div className="h-14 sm:h-16"></div>
+      {/* Ticker temporariamente removido para teste */}
+      {/* <BreakingNewsTicker /> */}
       
-      {/* Ticker - agora sem fixed, flui naturalmente */}
-      <BreakingNewsTicker />
-      
-      <main>
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          {children}
-        </div>
-      </main>
+      <div className="pt-16">
+        <main>
+          <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </main>
+      </div>
       
       <Footer />
     </div>
